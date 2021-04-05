2dd9018a421b9e90b252ea60acfc72e3d2ebfe8c
zf1976 <1596863112@qq.com>
2021-04-05 17:09:30 +0800

    fix：修复关闭加密bug

--- src/utils/request.js
+++ src/utils/request.js
@@ -22,10 +22,10 @@ service.interceptors.request.use(
       // 让每个请求携带自定义token 请根据实际情况自行修改
       config.headers.Authorization = getToken()
     }
-    let data = config.data
-    if (data) {
-      config.data = encryptByCBC(config.data)
-    }
+    // let data = config.data
+    // if (data) {
+    //   config.data = encryptByCBC(config.data)
+    // }
 
     if (Config.signaturePattern === 'OPEN') {
       config.params = {
