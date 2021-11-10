bbeae1fe62b14c00746ab6bd893538475ed85f54
zf1976 <1596863112@qq.com>
2021-11-10 18:55:42 +0800

    feat: 更新OAuth2客户端列表样式

--- src/views/components/safe/OAuthTableExpend.vue
+++ src/views/components/safe/OAuthTableExpend.vue
@@ -1,54 +1,95 @@
 
 <template>
-  <card style="width:570px">
+  <card>
     <p slot="title">
       <Icon type="ios-albums-outline" />
       详情
     </p>
-    <List size="small" :split=false>
-      <ListItem>
-        资源ID:&nbsp
-        <tag color="geekblue">
-          {{ row.resourceIds? row.resourceIds : '未知' }}
-        </tag>
-      </ListItem>
-      <ListItem label="">
-        认证方式:&nbsp
-        <tag color="geekblue">
-          {{ row.authorizedGrantTypes? row.authorizedGrantTypes:'未知' }}
-        </tag>
-      </ListItem>
-      <ListItem>
-        重定向:&nbsp
-        <tag color="geekblue">
-          {{ row.webServerRedirectUri? row.webServerRedirectUri: '未知' }}
-        </tag>
-      </ListItem>
-      <ListItem>
-        权限值:&nbsp
-        <tag color="geekblue">{{ row.authorities? row.authorities : '未知' }}</tag>
-      </ListItem>
-      <ListItem>
-        权限范围:&nbsp
-        <tag color="geekblue">{{ row.scope? row.scope : '未知' }}</tag>
-      </ListItem>
-      <ListItem>
-        Token有效期/秒:&nbsp
-        <tag color="geekblue">{{ row.accessTokenValidity? row.accessTokenValidity : '未知' }}</tag>
-      </ListItem>
-      <ListItem>
-        RefreshToken有效期/秒:&nbsp
-        <tag color="geekblue">{{ row.refreshTokenValidity? row.refreshTokenValidity : '未知' }}</tag>
-      </ListItem>
-      <ListItem>
-        预选属性/必须为JSON:&nbsp
-        <tag color="geekblue">{{ row.additionalInformation? row.additionalInformation : '未知' }}</tag>
-      </ListItem>
-      <ListItem>
-        自动批准:&nbsp
-        <tag color="geekblue">{{ row.autoApprove? row.autoApprove : '未知' }}</tag>
-      </ListItem>
-    </List>
+    <el-descriptions class="margin-top" direction="vertical" :column="2" border>
+      <el-descriptions-item>
+        <template slot="label">
+          <i class="el-icon-user"></i>
+          资源ID
+        </template>
+        <el-tag size="small">
+          {{ row.resourceIds? row.resourceIds : '无' }}
+        </el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template slot="label">
+          <i class="el-icon-mobile-phone"></i>
+          认证方式
+        </template>
+        <el-tag size="small">
+          {{ row.authorizedGrantTypes? row.authorizedGrantTypes:'无' }}
+        </el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template slot="label">
+          <i class="el-icon-location-outline"></i>
+          重定向URL
+        </template>
+        <el-tag size="small">
+          {{ row.webServerRedirectUri? row.webServerRedirectUri: '无' }}
+        </el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template slot="label">
+          <i class="el-icon-tickets"></i>
+          权限值
+        </template>
+        <el-tag size="small">
+          {{ row.authorities? row.authorities : '无' }}
+        </el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template slot="label">
+          <i class="el-icon-office-building"></i>
+          权限范围
+        </template>
+        <el-tag size="small">
+          {{ row.scope? row.scope : '无' }}
+        </el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template slot="label">
+          <i class="el-icon-office-building"></i>
+          Token有效期/秒
+        </template>
+        <el-tag size="small">
+          {{ row.accessTokenValidity? row.accessTokenValidity : '无' }}
+        </el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template slot="label">
+          <i class="el-icon-office-building"></i>
+          RefreshToken有效期/秒
+        </template>
+        <el-tag size="small">
+          {{ row.refreshTokenValidity? row.refreshTokenValidity : '无' }}
+        </el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template slot="label">
+          <i class="el-icon-office-building"></i>
+          预选属性/必须为JSON
+        </template>
+        <el-tag size="small">
+          {{ row.additionalInformation? row.additionalInformation : '无' }}
+        </el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template slot="label">
+          <i class="el-icon-office-building"></i>
+          自动授权
+        </template>
+        <el-tag size="small">
+          {{ row.autoApprove? row.autoApprove : '无' }}
+        </el-tag>
+      </el-descriptions-item>
+    </el-descriptions>
+
+
   </card>
 </template>
 <script>
